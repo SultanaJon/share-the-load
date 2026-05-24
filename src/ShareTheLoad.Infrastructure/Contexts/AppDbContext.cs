4ac69900d62d1299a4061c24189b7d24eacc42e0
= <jonsultana@outlook.com>
2026-05-24 13:14:33 -0400

    Set created at when new entities are added and set updated when enties are modified and the context is saved.

--- src/ShareTheLoad.Infrastructure/Contexts/AppDbContext.cs
+++ src/ShareTheLoad.Infrastructure/Contexts/AppDbContext.cs
@@ -14,4 +14,21 @@ public class AppDbContext : DbContext
         modelBuilder.ApplyConfigurationsFromAssembly(typeof(AppDbContext).Assembly);
         base.OnModelCreating(modelBuilder);
     }
+
+    public override Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
+    {
+        // Get all of the entities that inherit from BaseEntity
+        var entries = ChangeTracker.Entries<BaseEntity>();
+
+        foreach(var entry in entries)
+        {
+            if(entry.State == EntityState.Added)
+                entry.Entity.CreatedAt = DateTime.UtcNow;
+
+            if(entry.State == EntityState.Modified)
+                entry.Entity.UpdatedAt = DateTime.UtcNow;
+        }
+
+        return base.SaveChangesAsync(cancellationToken);    
+    }
 }
